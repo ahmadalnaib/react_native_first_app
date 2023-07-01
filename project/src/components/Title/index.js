afc59553a7109801f047bb58673eb5daf9a9dca3
ahmedhabeeb73 <alnaib888@gmail.com>
2023-07-01 14:45:15 +0200

    first commit

---
+++ project/src/components/Title/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {  Text } from "react-native";
+import styles from "./styles";
+
+const Title=()=>{
+  return(
+
+        <Text style={styles.title}>Title</Text>
+   
+  )
+}
+
+
+export default Title;
---
+++ project/src/components/Title/styles.js
@@ -0,0 +1,11 @@
+const { StyleSheet } = require("react-native");
+
+const styles=StyleSheet.create({
+  title:{
+    fontSize:24,
+    color:'blue' 
+  }
+})
+
+
+export default styles;
